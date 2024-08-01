18564a876080be8a89a8c0cdec497cd828050db5
Saad0095 <saadbinkhalid1895@gmail.com>
2024-08-01 12:03:00 -0700

    Made Navbar Responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className='flex justify-between px-10 items-center h-16 border-b-[1px]'>
-        <div className="logo font-bold text-lg text-blue-800">
-            JobSearch
+    <nav className="mb-4">
+      <div className="flex justify-between px-10 items-center min-h-16 border-b-[1px]">
+        <div className="logo font-bold text-lg text-blue-800">JobSearch</div>
+        <div
+          className="naviconBtn sm:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          &#9776;
         </div>
-      <ul className='flex justify-center items-center space-x-12 font-semibold'>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-        <NavLink to='/login'>Log In</NavLink>
-      </ul>
-    </nav>
-  )
-}
+        <ul className="hidden sm:flex sm:space-x-12 font-semibold">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/login">Log In</NavLink>
+        </ul>
+      </div>
 
-export default Navbar
+      {isOpen ? (
+        <ul className="flex flex-col sm:hidden font-semibold bg-gray-50 border-black border-b-[1px]">
+          <NavLink className="p-2" to="/" onClick={()=> setIsOpen(false)}>Home</NavLink>
+          <NavLink className="p-2" to="/contact" onClick={()=> setIsOpen(false)}>Contact</NavLink>
+          <NavLink className="p-2" to="/login" onClick={()=> setIsOpen(false)}>Log In</NavLink>
+        </ul>
+      ) : null}
+    </nav>
+  );
+};
+
+export default Navbar;
