be722fd7f29f723b98388dbbc624f66a9c26649e
Saad0095 <saadbinkhalid1895@gmail.com>
2024-08-06 12:51:47 -0700

    Added Readme and fixed some changes

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,29 +1,22 @@
-import React, { createContext, useContext,useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // useEffect(() => {
-  //   const storedAuthState = JSON.parse(localStorage.getItem('isAuthenticated'));
-  //   if (storedAuthState) {
-  //     setIsAuthenticated(true);
-  //   }
-  // }, []);
-
-  useEffect(() => {
-      setIsAuthenticated(JSON.parse(localStorage.getItem("isAuthenticated")))
-  }, [])
   
+  useEffect(() => {
+    setIsAuthenticated(JSON.parse(localStorage.getItem("isAuthenticated")));
+  }, []);
+
   const login = () => {
     setIsAuthenticated(true);
-    localStorage.setItem('isAuthenticated', JSON.stringify(true));
+    localStorage.setItem("isAuthenticated", JSON.stringify(true));
   };
 
   const logout = () => {
     setIsAuthenticated(false);
-    localStorage.setItem('isAuthenticated', JSON.stringify(false));
+    localStorage.setItem("isAuthenticated", JSON.stringify(false));
   };
 
   return (
