7d4bf83fabfe28dc68a1547a2d5f341982ccdaa8
Saad0095 <saadbinkhalid1895@gmail.com>
2024-08-07 03:18:58 -0700

    Stled Header and Footer

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { jobsData } from "./JobsData";
+import { TypeAnimation } from "react-type-animation";
 import { useNavigate } from "react-router-dom";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
@@ -24,25 +24,31 @@ const JobListing = () => {
 
   return (
     <div className="bg-white p-6 min-h-screen">
-      <h1 className="text-3xl font-bold mb-6 text-green-700 text-center">
-        Job Listings
-      </h1>
-      <div className="flex justify-center mb-6">
-        <div className="relative w-full md:w-2/3 lg:w-1/2">
-          <input
-            type="text"
-            placeholder="Search for jobs..."
-            value={searchQuery}
-            onChange={handleSearchChange}
-            className="border border-gray-300 p-3 pl-10 rounded-lg w-full focus:outline-none focus:border-green-500 shadow-md transition-all duration-300 ease-in-out"
+      <div className="header my-20">
+        <h1 className="text-4xl sm:text-5xl font-bold mb-6 text-green-950 text-center">
+          <TypeAnimation
+            sequence={["Find Your Dream Job!"]}
+            wrapper="span"
+            speed={50}
+            style={{ display: "inline-block" }}
           />
-          <FontAwesomeIcon
-            icon={faSearch}
-            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
-          />
+        </h1>
+        <div className="flex justify-center mb-6">
+          <div className="relative w-full md:w-2/3 lg:w-1/2">
+            <input
+              type="text"
+              placeholder="Search for jobs..."
+              value={searchQuery}
+              onChange={handleSearchChange}
+              className="border border-gray-300 p-3 pl-10 rounded-lg w-full focus:outline-none focus:border-green-500 shadow-md transition-all duration-300 ease-in-out"
+            />
+            <FontAwesomeIcon
+              icon={faSearch}
+              className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+            />
+          </div>
         </div>
       </div>
-
       {filteredJobs.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredJobs.map((job) => (
@@ -62,7 +68,10 @@ const JobListing = () => {
                 Salary: {job.salary}
               </p>
 
-              <button className="mt-4 bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition-colors duration-200" onClick={()=> navigate('/apply')}>
+              <button
+                className="mt-4 bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition-colors duration-200"
+                onClick={() => navigate("/apply")}
+              >
                 Apply Now
               </button>
             </div>
