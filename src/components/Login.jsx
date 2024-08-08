1fbbac89ba51fdd302c3048aad19fc5e9e0fa41a
Saad0095 <saadbinkhalid1895@gmail.com>
2024-08-08 03:46:56 -0700

    Added Show and Hide feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const Login = () => {
   const navigate = useNavigate();
-  const {login} = useAuth();
-  
+  const { login } = useAuth();
+  const [showPassword, setShowPassword] = useState(false);
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
 
   const [errorMsg, setErrorMsg] = useState(null);
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -36,12 +40,18 @@ const Login = () => {
 
   return (
     <div className="my-10 h-[70vh] flex flex-col justify-center items-center">
-      <h1 className=" text-center text-green-700 font-bold text-3xl m-3">Login</h1>
-      <form action="" className="mx-auto w-3/4 md:w-1/3">
+      <h1 className=" text-center text-green-700 font-bold text-3xl m-3">
+        Login
+      </h1>
+      <form
+        action=""
+        className="mx-auto w-3/4 md:w-1/3"
+        onSubmit={handleSubmit}
+      >
         <p className="text-red-500">{errorMsg}</p>
         <div className="flex flex-col w-full my-4">
           <label htmlFor="email" className="font-semibold mb-2">
-            Email: 
+            Email:
           </label>
           <input
             value={formData.email}
@@ -53,18 +63,26 @@ const Login = () => {
         </div>
         <div className="flex flex-col w-full my-4">
           <label htmlFor="password" className="font-semibold mb-2">
-            Password: 
+            Password:
           </label>
-          <input
-            value={formData.password}
-            name="password"
-            onChange={handleChange}
-            className="border border-gray-300 rounded p-2 text-sm bg-gray-50 outline-blue-500"
-            type="password"
-          />
+          <div className="password-field relative">
+            <input
+              value={formData.password}
+              name="password"
+              onChange={handleChange}
+              className="w-full border border-gray-300 rounded p-2 text-sm bg-gray-50 outline-blue-500"
+              type={showPassword ? "text" : "password"}
+            />
+            <span onClick={() => setShowPassword(!showPassword)} className="absolute right-2 top-2 hover:cursor-pointer">
+              {showPassword ? (
+                <FontAwesomeIcon icon={faEyeSlash} />
+              ) : (
+                <FontAwesomeIcon icon={faEye} />
+              )}
+            </span>
+          </div>
         </div>
         <button
-          type="submit"
           className="bg-green-500 hover:bg-green-600 text-white font-semibold px-10 py-2 mx-auto my-6 rounded flex items-center justify-center"
           onClick={handleSubmit}
         >
